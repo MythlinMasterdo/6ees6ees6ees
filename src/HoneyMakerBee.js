f1abe6abe63d9e6e7921e3c64bb054897092ca1e
Miles Smith <miles71397@gmail.com>
2016-08-21 11:35:55 -0700

    Completed

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,7 +1,19 @@
 import Bee from './Bee'
 
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = "make honey";
+    this.honeyPot = 0;
+    this.makeHoney = function() {
+      this.honeyPot++;
+    }
+    this.giveHoney = function() {
+      this.honeyPot--;
+    }
+  }
+
 };
 
 export default HoneyMakerBee;
